8ca0547e331c36f3c5e8db841c6aca2006590577
peter.mbogo@student.moringaschool.com <peter.mbogo@student.moringaschool.com>
2022-11-24 02:04:39 +0300

    Completed assignment

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,5 @@
 class AuthorsController < ApplicationController
+  rescue_from ActiveRecord::RecordInvalid, with: :render_when_record_invalid
   
   def show
     author = Author.find(params[:id])
@@ -7,15 +8,19 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    author = Author.create(author_params)
+    author = Author.create!(author_params)
 
-    render json: author, status: :created
+    render json: author, status: :created, status: 201
   end
 
   private
   
   def author_params
     params.permit(:email, :name)
+  end
+
+  def render_when_record_invalid(invalid)
+    render json: { errors: invalid.record.errors}, status: 422
   end
   
 end
